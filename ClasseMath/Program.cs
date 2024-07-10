af648656270f40af9dde5e87318dfe38788f8bca
Alexandre Abreu <contatoalexandre95@gmail.com>
2024-07-10 15:35:28 -0300

    Learn about System.Math library

---
+++ ClasseMath/Program.cs
@@ -0,0 +1,7 @@
+﻿//Implementar um método da classe Math que retorna o maior de dois números.
+
+int firstValue = 888;
+int secondValue = 999;
+int largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
