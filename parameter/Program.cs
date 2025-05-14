ceff96acc389b50bbdfa75e9155a4ead7ca46e17
Alexandre Abreu <contatoalexandre95@gmail.com>
2025-05-14 15:01:03 -0300

    initializing a project with paramater argumets.

---
+++ parameter/Program.cs
@@ -0,0 +1,13 @@
+﻿
+/*Neste exemplo, o método CountTo aceita um parâmetro inteiro chamado max. O parâmetro é referenciado no for do loop
+ *do método. Quando CountTo é chamado, o inteiro 5 é fornecido como argumento.*/
+
+CountTo(5);
+
+void CountTo(int max)
+{
+    for(int i = 0; i < max; i++)
+    {
+        Console.Write($"{i}, ");
+    }
+}
